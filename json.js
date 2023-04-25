4e790a564549912bc33990ac98bdcf9522fb8a15
Philipp K <114767980+PhilNorseMan@users.noreply.github.com>
2023-04-26 00:06:14 +0300

    HW11

---
+++ json.js
@@ -0,0 +1,59 @@
+export const products = `[
+
+    {
+        "id": "1",
+        "img": "img/product1.jpg",
+        "name": "ELLERY X M 'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52 .00"
+    },
+    {
+        "id": "2",
+        "img": "img/product2.jpg",
+        "name": "ELLERY X M 'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52 .00"
+    },
+    {
+        "id": "3",
+        "img": "img/product3.jpg",
+        "name": "ELLERY X M 'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52 .00"
+    },
+    {
+        "id": "4",
+        "img": "img/product4.jpg",
+        "name": "ELLERY X M 'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52 .00"
+    },
+    {
+        "id": "5",
+        "img": "img/product5.jpg",
+        "name": "ELLERY X M 'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52 .00"
+    },
+    {
+        "id": "6",
+        "img": "img/product6.jpg",
+        "name": "ELLERY X M 'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52 .00"
+    },
+    {
+        "id": "TEST 1",
+        "img": "img/test1.jpg",
+        "name": "TEST PRODUCT",
+        "text": "Testing JSON",
+        "price": "$0 .00"
+    },
+    {
+        "id": "TEST 2",
+        "img": "img/testing.jpg",
+        "name": "TEST PRODUCT",
+        "text": "Testing JSON 2",
+        "price": "$100500 .00"
+    }
+]`;
